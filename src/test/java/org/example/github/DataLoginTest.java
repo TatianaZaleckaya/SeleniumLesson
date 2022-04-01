1c1835a499546d3fe73d54c44583919e07f5b74b
TatianaZaleckaya <80416820+TatianaZaleckaya@users.noreply.github.com>
2022-04-01 08:47:21 +0300

    Update DataLoginTest.java
    delete data

--- src/test/java/org/example/github/DataLoginTest.java
+++ src/test/java/org/example/github/DataLoginTest.java
@@ -3,8 +3,8 @@ package org.example.github;
 public enum DataLoginTest {
 
     POSITIVE ("nekhaevna2212@gmail.com","MIStati2212"),
-    INVALID_PASSWORD ("nekhaevna2212@gmail.com", "3r4g45gt"),
-    INVALID_LOGIN("   ","MIStati2212");
+    INVALID_PASSWORD ("", "3r4g45gt"),
+    INVALID_LOGIN("   ","");
 
     String login;
     String password;
